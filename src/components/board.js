b7cb82bd426e74933b50d4c1e5c131cd85befb57
Alexa Scott <arscott4792@gmail.com>
2018-09-05 13:46:33 -0700

    rendering lists and setting state

--- src/components/board.js
+++ src/components/board.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-
+import List from './list';
 import AddForm from './add-form';
 
+import './board.css';
+
 class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lists: []
+        };
+    }
+
+    addList(title) {
+        this.setState({
+            lists: [...this.state.lists, {title}]
+        });
+    }
     render() {
-        console.log(this.props);
+        console.log(this.state);
+        const lists = this.state.lists.map((list, index) => (
+            <li className="list-wrapper" key={index}>
+                <List {...list}/>
+            </li>
+        ));
+        console.log(lists);
         return(
             <div className="board">
                 <h2>{this.props.title}</h2>
                 <ul className="lists">
-                <li className="list-wrapper">This will be a LIST</li>
+                    {lists}
                     <li className="add-list-wrapper">
                         <AddForm 
-                        type="list"/>
+                        type="list"
+                        onAdd={text => this.addList(text)}/>
                     </li>
                 </ul>
             </div>
